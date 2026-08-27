cf0bab56a723c398740850bc9870d578df62cf17
Andrii Miroshnyk <miroshnykandrii@gmail.com>
2026-08-27 12:10:07 +0300

    Remove config_version from the code

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -7,11 +7,6 @@ variable "project_config_path" {
     condition     = fileexists(var.project_config_path)
     error_message = "project_config_path must point to an existing file."
   }
-
-  validation {
-    condition     = try(jsondecode(file(var.project_config_path)).config_version == 3, false)
-    error_message = "project_config_path must contain valid JSON using config_version 3."
-  }
 }
 
 variable "secret_version_managers" {
