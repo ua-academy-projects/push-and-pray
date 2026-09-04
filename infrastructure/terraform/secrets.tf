55bbb2ddddd27f594b2c58c8f8e0fed03d623ab7
Andrii Isopenko <andrii.isopenko@gmail.com>
2026-09-04 13:44:32 +0300

    Delete <moved_to> blocks

--- infrastructure/terraform/secrets.tf
+++ infrastructure/terraform/secrets.tf
@@ -12,28 +12,3 @@ module "aws_secrets" {
   config         = local.config
   iam_role_names = module.aws_vm.iam_role_names
 }
-
-moved {
-  from = module.gcp_secrets[0]
-  to   = module.gcp_secrets
-}
-
-moved {
-  from = module.aws_secrets[0]
-  to   = module.aws_secrets
-}
-
-moved {
-  from = google_secret_manager_secret.this
-  to   = module.gcp_secrets.google_secret_manager_secret.this
-}
-
-moved {
-  from = google_secret_manager_secret_iam_member.workload_access
-  to   = module.gcp_secrets.google_secret_manager_secret_iam_member.workload_access
-}
-
-moved {
-  from = google_secret_manager_secret_iam_member.version_adder
-  to   = module.gcp_secrets.google_secret_manager_secret_iam_member.version_adder
-}
